4aaa19feaec0adfc010e0b0ccade15ff2e02bcee
eug <18398188+EuHello@users.noreply.github.com>
2024-03-21 21:44:10 +0800

    Add docstrings to all functions in lambda hander

--- telegram_bot/app.py
+++ telegram_bot/app.py
@@ -14,6 +14,15 @@ BOT_APP = 'BOT_APP: '
 
 
 def create_user(data: dict):
+    """
+    Creates new TelegramUser instance, from data message.
+
+    Params:
+        data: dict, data from event / Telegram API Webhook
+
+    Returns:
+        user: TelegramUser instance
+    """
     user = TelegramUser(user_id=data['from']['id'],
                         username = data['from']['username'],
                         first_name = data['from']['first_name'],
@@ -24,6 +33,16 @@ def create_user(data: dict):
 
 
 def validate_user(user: TelegramUser):
+    """
+    Validates TelegramUser instance. Checks whether lambda handler should act on the user.
+
+    Params:
+        user: TelegramUser instance
+
+    Returns:
+        True:  bool, if user is valid
+        False: bool, otherwise
+    """
     if user.is_bot:
         return False
     if user.chat_id < 0:
@@ -38,6 +57,17 @@ def parse_text():
 
 
 def reply_user(chat_id: int, message: str):
+    """
+    Wrapper function that responds a message to TelegramUser.
+
+    Params:
+        chat_id: int, chat_id of the user
+        message: str, message to reply to the user
+
+    Returns:
+        True:  bool, if no issue found
+        False: bool, if telegram bot token is None. i.e. not retrieved.
+    """
     token = get_bot_credentials()
     if token is None:
         logger.info(f"{BOT_APP}Invalid Token")
@@ -48,17 +78,48 @@ def reply_user(chat_id: int, message: str):
 
 
 def chat_user(chat_id: int, text: str, chat_url):
+    """
+    Performs a POST request to Telegram API.
+
+    Params:
+        chat_id:  int, chat_id of the user
+        text:     str, text message to reply to the user
+        chat_url: str, Telegram API endpoint
+
+    Returns:
+        json:     json response from Telegram API
+    """
     params = {"chat_id": chat_id, "text": text}
     response = requests.post(chat_url, json=params)
     return response.json()
 
 
 def get_bot_url(credentials):
+    """
+    Construct Telegram API URL based on Bot's credentials.
+
+    Params:
+        credentials: str, Telegram Bot's private token
+
+    Returns:
+        url: str, Telegram API endpoint
+    """
     url = f'{BOT_BASE_URL}{credentials}/{BOT_METHOD}'
     return url
 
 
-def get_bot_credentials(bot_name=BOT_TOKEN):
+def get_bot_credentials(token_name=BOT_TOKEN):
+    """
+    Get secret Telegram Bot Token/Credential from AWS Parameter Store.
+    Uses AWS Parameters and Secrets Lambda Extension, that allows caching.
+
+    Params:
+        token_name: str, name of the token, from config.py
+
+    Returns:
+        secret: str, secret token from AWS Parameter Store
+        None:   None, if token_name is not correct
+    """
     secret_ext_endpoint = (f'http://localhost:{SECRET_EXT_PORT}/systemsmanager/parameters/'
                            f'get?name={TOKEN_PARAM_PATH}&withDecryption=true')
     request_ssm = urllib.request.Request(secret_ext_endpoint)
@@ -68,18 +129,17 @@ def get_bot_credentials(bot_name=BOT_TOKEN):
     secret_name = json.loads(resp)['Parameter']['Name']
     secret = json.loads(resp)['Parameter']['Value']
 
-    if secret_name == bot_name:
+    if secret_name == token_name:
         return secret
 
     return None
 
 
-def lambda_handler(event, context):
+def lambda_handler(event: dict, context: object):
     """
-    Lambda function that reads text and replies user after validation
+    Lambda handler that receives events from API Gateway. Performs action for users, and responds back to API Gateway.
 
-    Params
-    ----------
+    Params:
     event: dict, required
         API Gateway Lambda Proxy Input Format
         Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
@@ -88,9 +148,8 @@ def lambda_handler(event, context):
         Lambda Context runtime methods and attributes
         Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
 
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
+    Returns:
+        API Gateway Lambda Proxy Output Format: dict
         Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
     """
 
