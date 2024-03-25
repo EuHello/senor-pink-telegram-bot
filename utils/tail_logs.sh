3accf8454e0a3531bc2a8aaaffb45bfc4203b0de
eug <18398188+EuHello@users.noreply.github.com>
2024-03-25 21:51:33 +0800

    Update filters in script tail_logs.sh

--- utils/tail_logs.sh
+++ utils/tail_logs.sh
@@ -3,10 +3,10 @@
 
 source utils/config.sh
 
-echo "Options: -b          See only bot app logs"
+echo "Options: -err          Filter for error logs"
 
-if [[ $1 == "-b" ]]; then
-  cmd1="sam logs -n TelegramBot --stack-name senor-pink-telegram-bot --profile $MYPROFILE --tail -s '5min ago' --filter \"BOT_APP\""
+if [[ $1 == "-err" ]]; then
+  cmd1="sam logs -n TelegramBot --stack-name senor-pink-telegram-bot --profile $MYPROFILE --tail -s '5min ago' --filter \"err\""
   echo "$cmd1"
   $cmd1
 else
